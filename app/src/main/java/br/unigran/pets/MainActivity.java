2db2c0117923627f989e3f891421bc8b5ae2d5ca
kat2804 <60763154+kat2804@users.noreply.github.com>
2020-12-17 21:56:51 -0300

    segundo commit

--- app/src/main/java/br/unigran/pets/MainActivity.java
+++ app/src/main/java/br/unigran/pets/MainActivity.java
@@ -1,14 +1,69 @@
 package br.unigran.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private  RecyclerView.LayoutManager layoutManager;
 
-    @Override
+    private ListView list_view;
+    private List <Cachorro> cachorroList;
+    private Integer indice;
+
+    public class Cachorro{
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String name;
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public String getRaca() {
+            return raca;
+        }
+
+        public void setRaca(String raca) {
+            this.raca = raca;
+        }
+
+        public int idade;
+        public String raca;
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.list_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
     }
+
+
+
 }
